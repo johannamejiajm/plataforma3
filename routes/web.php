e2a2334f9af926f96fdd1fe8fc16432db348a216
Alexander0711 <alexanderrangelflorez@gmail.com>
2025-04-24 21:22:25 -0500

    se agg  $data en el controlador para que guarde un artista en estado 0

--- routes/web.php
+++ routes/web.php
@@ -46,6 +46,7 @@ Route::middleware(['auth'])->group(function () {
 
 
 
+
 /* Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard'); */
