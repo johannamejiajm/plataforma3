cc6fe6c01bf3d726bf9e45640339e89eb084ebc1
juansebastian0816 <contrerasvanegas0816@gmail.com>
2025-06-04 22:01:51 -0500

    cambie formulario por el de donaciones

--- app/Http/Controllers/ArtistasController.php
+++ app/Http/Controllers/ArtistasController.php
@@ -11,9 +11,7 @@ use Symfony\Contracts\Service\Attribute\Required;
 class ArtistasController extends Controller
 {
 
-    /**
-     * Display a listing of the resource.
-     */
+    
 
 
     public function listarArtistasActivos()
@@ -70,25 +68,21 @@ class ArtistasController extends Controller
 
         return redirect()->route('artistas.index')->with('success', 'El estado del artista ha sido actualizado.');
 
-        //$artistas = artistas::all();
-        //return view('publico/vistas/artistas/inscripciones', compact('artistas'));
+       
 
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    
     public function create()
     {
         $eventos = Evento::all(); // obtener eventos para el select
         return view('publico/vistas/artistas/create', compact('eventos'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    
     public function store(Request $request)
     {
+        
 
         DB::beginTransaction();
         try {
@@ -104,13 +98,23 @@ class ArtistasController extends Controller
                 'estado' => 'required|in:1,0', // ahora acepta directamente 1 o 0
 
             ], [
+                'email.required' => 'El correo es obligatorio.',
+                'email.email' => 'El correo debe tener la siguiente estructura ejemplo@ejem.ejem.',
+                'email.unique' => 'El correo ya existe.',
+                'descripcion.required' => 'La descripción es obligatoria.',
+                'descripcion.string' => 'La descripción debe ser texto.',
+                'descripcion.max' => 'La descripción no puede ser mayor a :max caracteres.',
+                'nombre.required' => 'El nombre es obligatorio.',
+                'nombre.string' => 'El nombre debe ser texto.',
+                'idevento.required' => 'El evento es obligatorio.',
+                'identidad.required' => 'El numero de identidad es obligatorio.',
                 'identidad.required' => 'El campo número de identidad es obligatorio.',
                 'identidad.unique' => 'Este número de identidad ya está registrado.',
                 'identidad.max' => 'El número de identidad no debe tener más de 20 caracteres.',
                 'imagen.max' => 'El campo de imagen no debe ser mayor a 2048 kilobytes.',
             ]);
             // Conversión del estado a entero (por si viene como string)
-            // $validated['estado'] = (int)$request->estado;
+            
 
             // Guardar la imagen si se subió
             if ($request->hasFile('imagen')) {
@@ -155,34 +159,26 @@ class ArtistasController extends Controller
         // Redirigir o enviar una respuesta
         return redirect()->route('artistas.create')->with('success', 'Artista creado correctamente.');
     }
-    /**
-     * Display the specified resource.
-     */
+    
     public function show(Artistas $artistas)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+    
     public function edit(Artistas $artistas)
     {
         $artistas = Artistas::find($artistas->id);
         return view("admin/vistas/artistas/editartistas", compact('artistas'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+    
     public function update(Request $request, Artistas $artistas)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+    
     public function destroy(Artistas $artistas)
     {
         //
