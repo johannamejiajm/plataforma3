337126ae8a23c806f4f4f62aafa523b6b173b67f
zebaz990521 <zebaz.21cas@gmail.com>
2025-06-09 21:41:16 -0500

    asigne los roles a con sus permisos

--- database/seeders/RolesAndPermissionsUser.php
+++ database/seeders/RolesAndPermissionsUser.php
@@ -2,10 +2,15 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Support\Carbon;
 
 class RolesAndPermissionsUser extends Seeder
 {
@@ -35,5 +40,47 @@ class RolesAndPermissionsUser extends Seeder
          $adminRole->givePermissionTo([$managePubliaciones, $manageInformacion, $manageRoles, $managePermisos, $manageUsers, $manageEventos, $manageContactos, $manageArtistas]);
          $publicadorRole->givePermissionTo([$managePubliaciones, $manageArtistas, $manageEventos, $manageInformacion]);
          $tesoreroRole->givePermissionTo($manageDonaciones);
+
+          DB::table('users')->insert([
+            [
+                'name' => 'Administrador',
+                'email' => 'administrador@gmail.com',
+                'email_verified_at' => Carbon::now(),
+                'password' => Hash::make('admin123456789'),
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Publicador',
+                'email' => 'publicador@gmail.com',
+                'email_verified_at' => Carbon::now(),
+                'password' => Hash::make('publicador123456789'),
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+             [
+                'name' => 'tesorero',
+                'email' => 'tesorero@gmail.com',
+                'email_verified_at' => Carbon::now(),
+                'password' => Hash::make('tesorero123456789'),
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+
+        // Obtener usuarios por su correo electrónico
+        $admin = User::where('email', 'administrador@gmail.com')->first();
+        $publicador = User::where('email', 'publicador@gmail.com')->first();
+        $tesorero = User::where('email', 'tesorero@gmail.com')->first();
+
+        // Asignar roles
+        $admin->assignRole($adminRole);
+        $publicador->assignRole($publicadorRole);
+        $tesorero->assignRole($tesoreroRole);
+
     }
 }
+
