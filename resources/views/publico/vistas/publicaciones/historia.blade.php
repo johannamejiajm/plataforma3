7850e0cd5022d44b761dfd5800b50bafc653feb9
johannamejiajm <johannamejiajm2@gmail.com>
2025-06-23 13:22:40 -0500

    ajuste ver fotos

--- resources/views/publico/vistas/publicaciones/historia.blade.php
+++ resources/views/publico/vistas/publicaciones/historia.blade.php
@@ -67,7 +67,7 @@
             @foreach ($historias as $key => $historia)
                 <div class="card-historia">
                     <div class="card-historia-img" data-bs-toggle="modal" data-bs-target="#modalHistoria{{ $key }}" style="cursor: pointer;">
-                        <img src="{{ asset($historia->fotos[0]->imagen) }}" alt="Imagen relacionada a {{ $historia->titulo }}">
+                        <img src="{{ asset($historia?->fotos[0]?->imagen) }}" alt="Imagen relacionada a {{ $historia->titulo }}">
                     </div>
                     <div class="card-historia-body">
                         <p class="categoria">Historia</p>
